473d7515f6c36834ad71522b2a13dbccc7e455b6
dinoman12345 <79095574+dinoman12345@users.noreply.github.com>
2021-08-19 04:08:13 -0700

    Add files via upload

---
+++ activity1.js
@@ -0,0 +1,12 @@
+x=0
+function mckpnxpjii(){
+x=x+1;
+document.getElementById("score").innerHTML="Score-"+x;3
+}
+function helmet(){
+    localStorage.setItem("dino",x);
+}
+function you(){
+    window.location="index1.html";
+
+}
---
+++ name1.js
@@ -0,0 +1,7 @@
+function you1(){
+    window.location="index1.html";
+}
+function helmet1(){
+y=localStorage.getItem("dino");
+document.getElementById("update").innerHTML = "<h1>Score: " + y +"</h1>";
+}
